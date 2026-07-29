e40a8b0656d06af6330a2c2d927c790449f03646
YP <yagnik.p@betaara.com>
2026-07-29 13:43:44 +0530

    feat: create GameCard component with play functionality and authentication check

--- src/app/shared/components/game-card/game-card.ts
+++ src/app/shared/components/game-card/game-card.ts
@@ -2,6 +2,9 @@ import { Component, inject, Input } from '@angular/core';
 import { Common } from '../../../core/services/common';
 import { AuthService } from '../../../core/services/auth';
 import { Api } from '../../../core/services/api';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Login } from '../../../authentication/login/login';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-game-card',
@@ -17,6 +20,8 @@ export class GameCard {
   public commonService = inject(Common);
   public apiService = inject(Api);
   public authService = inject(AuthService);
+  private modalService = inject(NgbModal);
+  private toastr = inject(ToastrService);
 
   getGameThumbnail(): string {
     if (this.type === 'casino') {
@@ -33,6 +38,13 @@ export class GameCard {
   }
 
   playGame() {
+    if (!this.authService.isLoggedIn() || !this.authService.getToken()) {
+      this.toastr.warning('Please log in to play the game.');
+      this.modalService.dismissAll();
+      this.modalService.open(Login, { centered: true, backdrop: 'static' });
+      return;
+    }
+
     const user = this.authService.currentUser();
     const player = {
       game_id: this.game?.gid,
